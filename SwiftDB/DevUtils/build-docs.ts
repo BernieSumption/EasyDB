ea8bd38c733d03f1091156684947019062eee096
Bernie Sumption <bernie@berniecode.com>
2022-04-13 07:52:58 +0100

    WIP

--- SwiftDB/DevUtils/build-docs.ts
+++ SwiftDB/DevUtils/build-docs.ts
@@ -1,4 +1,37 @@
 import fs from "fs"
-import { sourceFile } from "./utils"
+import { fatalError, markerName, sourceFile as sourcePath } from "./utils"
 
-const readmePath = sourceFile("README.md")
+const readmePath = sourcePath("README.md")
+
+const content = fs.readFileSync(readmePath, "utf8")
+
+content.replace(/<!---([\w-]+)--->\s*\n```swift\n((?:[\s\S](?!```))+)\n```/mg, (match, name, code) => {
+    if (code.includes("<!") || code.includes("->")) fatalError(`Code block includes comment marker:\n${match}`)
+    if (code.includes("```")) fatalError(`Code block includes block marker:\n${match}`)
+    
+    console.log("!!!", name)
+    console.log("???", code)
+    return ""
+})
+
+// let marker: string | undefined
+
+// for (let i = 0; i < lines.length; ++i) {
+//     const line = lines[i]
+//     const lineMarker = markerName(line)
+//     if (lineMarker) {
+//         ++i
+//         if (lines[i] !== "```swift") fatalError(`Expected start of code block at line ${i+1} got "${lines[i]}"`)
+//         ++i
+//         let code = ""
+//         const firstCodeLine = i
+//         while (true) {
+//             if (lines[i].trim() !== "```") {
+//                 code += lines[i] + "\n"
+//             } else {
+//                 const lastCodeLine
+//             }
+//         }
+//     }
+// }
+
--- SwiftDB/DevUtils/utils.ts
+++ SwiftDB/DevUtils/utils.ts
@@ -1,3 +1,19 @@
 import path from "path"
 
 export const sourceFile = (file: string) => path.join(__dirname, "..", file)
+
+export const markerName = (line: string) => {
+    line = line.trim()
+    if (line.includes("<!")) {
+        const match = (line.match(/^<!---([\w-]+)--->$/) || [])[1]
+        if (match) {
+            return match
+        }
+        fatalError(`Line contains comment marker but doesn't match marker pattern: ${line}`)
+    }
+}
+
+export const fatalError = (message: string) => {
+    console.log(message)
+    process.exit(1)
+}
